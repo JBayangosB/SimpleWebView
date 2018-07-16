e5709b849b668096c33bc7dbe7d656077a76b46b
Jayden Bayangos-Brandt <jayden@ittybittyapps.com>
2018-07-17 09:22:59 +1000

    Basic setup of webview

--- app/src/main/java/jbayangosb/simplewebview/MainActivity.kt
+++ app/src/main/java/jbayangosb/simplewebview/MainActivity.kt
@@ -2,11 +2,37 @@ package jbayangosb.simplewebview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.CookieManager
+import android.webkit.WebChromeClient
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val BASE_URL = "https://www.fastmail.com/"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupView()
+    }
+
+    private fun setupView() {
+        CookieManager.getInstance().setAcceptCookie(true)
+        webView.apply {
+            webViewClient = WebViewClient()
+            webChromeClient = WebChromeClient()
+        }
+        webView.settings.apply {
+            javaScriptEnabled = true
+            allowFileAccess = true
+            allowContentAccess = true
+            domStorageEnabled = true
+            allowFileAccessFromFileURLs = true
+            allowUniversalAccessFromFileURLs = true
+        }
+        webView.loadUrl(BASE_URL)
     }
 }
